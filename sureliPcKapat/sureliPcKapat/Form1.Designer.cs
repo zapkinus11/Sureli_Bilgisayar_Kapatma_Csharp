a5adfe5c468000a8159af3381405e121ac388201
Ahmet <110814262+zapkinus11@users.noreply.github.com>
2024-02-04 23:37:53 +0300

    c# süreli bilgisayar kapatma
    c# ile yazılmış süreli bilgisayar kapatma uygulaması

---
+++ sureliPcKapat/sureliPcKapat/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace sureliPcKapat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.dkAyarlama = new System.Windows.Forms.NumericUpDown();
+            this.btnOnayla = new System.Windows.Forms.Button();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dkAyarlama)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(210, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Süre Seçiniz:";
+            // 
+            // dkAyarlama
+            // 
+            this.dkAyarlama.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.dkAyarlama.Location = new System.Drawing.Point(241, 145);
+            this.dkAyarlama.Name = "dkAyarlama";
+            this.dkAyarlama.Size = new System.Drawing.Size(125, 37);
+            this.dkAyarlama.TabIndex = 1;
+            // 
+            // btnOnayla
+            // 
+            this.btnOnayla.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnOnayla.Location = new System.Drawing.Point(168, 214);
+            this.btnOnayla.Name = "btnOnayla";
+            this.btnOnayla.Size = new System.Drawing.Size(123, 70);
+            this.btnOnayla.TabIndex = 2;
+            this.btnOnayla.Text = "İşlemi Başlat";
+            this.btnOnayla.UseVisualStyleBackColor = true;
+            this.btnOnayla.Click += new System.EventHandler(this.btnOnayla_Click);
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnCikis.Location = new System.Drawing.Point(226, 304);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(161, 55);
+            this.btnCikis.TabIndex = 3;
+            this.btnCikis.Text = "Çıkış";
+            this.btnCikis.UseVisualStyleBackColor = true;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCancel.Location = new System.Drawing.Point(322, 214);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(119, 70);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "İptal Et";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label4.Location = new System.Drawing.Point(197, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(244, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "(kaç dakika sonra kapansın?)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 447);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.btnOnayla);
+            this.Controls.Add(this.dkAyarlama);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Otomatik Bilgisayar Kapatma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dkAyarlama)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown dkAyarlama;
+        private System.Windows.Forms.Button btnOnayla;
+        private System.Windows.Forms.Button btnCikis;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ sureliPcKapat/sureliPcKapat/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sureliPcKapat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnOnayla_Click(object sender, EventArgs e)
+        {
+            if(dkAyarlama.Value>0)
+            {
+                int kapanmaSuresi = (int)dkAyarlama.Value;
+                Process.Start("shutdown", $"/s /t {kapanmaSuresi * 60}");
+                MessageBox.Show($"{kapanmaSuresi} dakika sonra bilgisayar kapanacaktır!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (dkAyarlama.Value <= 0)
+            {
+                MessageBox.Show("Lütfen süre seçimi yapınız!", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı problemli hata!", "Büyük Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if(dkAyarlama.Value > 0)
+            {
+                Process.Start("shutdown", "/a");
+                MessageBox.Show("İşlem iptal edilmiştir!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if(dkAyarlama.Value <= 0)
+            {
+                MessageBox.Show("Lütfen süre seçimi yapınız!", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen süre seçimi yapınız!", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            string pcAdi = Environment.MachineName;
+            MessageBox.Show($"Uygulamamı kullandığın için teşekkürler {pcAdi}", "Ahmet TOPAL's Application", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
